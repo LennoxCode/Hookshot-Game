7330a142cc79ce9238884165ad887721eee019cf
Jannes <jannes.luca@hotmail.com>
2022-05-28 19:21:03 +0200

    commented a few classes and grapplehook sprite now gets moved with grapplepoint

--- Assets/Scripts/AimScript.cs
+++ Assets/Scripts/AimScript.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Rotates the Hook according to players mouse position.
+/// </summary>
 public class AimScript : MonoBehaviour
-{
+{   
     private Camera _cam;
     // Start is called before the first frame update
     void Start()
