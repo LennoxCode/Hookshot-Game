cf46090393cd0a32fcf21626deba73412481fb47
LennoxCode <leonard.valentin1999@gmail.com>
2022-05-30 17:47:47 +0200

    added prefab for random coin generation

---
+++ Assets/Scripts/CoinSpawnerArea.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+
+public class CoinSpawnerArea : MonoBehaviour
+{
+    [SerializeField] private GameObject coinPrefab;
+    [SerializeField] [Range(5, 60)] private int maxCoins;
+    [SerializeField] private int coinSeed;
+    
+    void Start()
+    {
+        Random.InitState(coinSeed);
+        Transform t = transform;
+        for (int i = 0; i < maxCoins; i++)
+        {
+            Vector2 pos = t.position;
+            Vector2 scale = t.localScale / 2;
+            float x = Random.Range(pos.x - scale.x, pos.x + scale.x);
+            float y = Random.Range(pos.y - scale.y, pos.y + scale.y);
+            Instantiate(coinPrefab, new Vector3(x, y, 0), Quaternion.identity);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawCube(transform.position, transform.localScale);
+        
+    }
+}
